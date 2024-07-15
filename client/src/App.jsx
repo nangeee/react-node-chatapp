4a2f20c6512aac36ce953b7811c43efc63d16183
nangeee <miniwoninuna@gmail.com>
2024-07-15 20:30:55 +0900

    feat: Implement login and signup screens using Shadcn UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
-import { Button } from "@/components/ui/button"
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
+import Auth from './pages/auth'
+import Chat from './pages/chat'
+import Profile from './pages/profile'
 
 const App = () => {
   return (
-    <div>
-      <Button>Click me: Shadcn</Button>
-      <br></br>
-      <button>Click me: default</button>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/auth" element={ <Auth /> } /> {/* 'auth' 경로로 들어오면 Auth 페이지로 라우팅 */}
+      <Route path="/chat" element={ <Chat /> } />
+      <Route path="/profile" element={ <Profile /> } />
+
+      <Route path="*" element={ <Navigate to="/auth" /> } /> {/* 정의되어 있지 않은 경로로 들어오면 auth 페이지로 navigate(Redirection) */}
+    </Routes>
+    </BrowserRouter>
   )
 }
 
