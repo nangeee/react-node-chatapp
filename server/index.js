a4c21d71e09c6aac7c0df8c7b4ac737a1d9d2cdf
nangeee <miniwoninuna@gmail.com>
2024-07-22 15:15:10 +0900

    feat: connect Node.js to MongoDB

--- server/index.js
+++ server/index.js
@@ -5,3 +5,13 @@ import cookieParser from 'cookie-parser'
 import mongoose from 'mongoose'
 
 dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+const databaseURL = process.env.DATABASE_URL;
+
+const server = app.listen(port, ()=>{
+  console.log(`Server is running at http://localhost:${port}`);
+})
+
+mongoose.connect(databaseURL).then(()=>console.log("DB connection succeeded!!")).catch(err=>console.log(err.message)); // DB connection 과정에서 에러 발생할 경우 catch
